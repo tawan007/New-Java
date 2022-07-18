9b6e183b8d649cef71f8ede25102080c855b0d2b
tawan meekairach <tawan.mee@ku.th>
2022-07-18 19:21:12 +0700

    Update index.java

--- index.java
+++ index.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 15;
-        int z = x + y;
-        System.out.print(x);
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.print("Enter Score Math: ");
+        int Scoremath = Keyboard.nextInt();
+        System.out.println("Enter Score Science");
+        int ScoreScience = Keyboard.nextInt();
+        System.out.println("Enter Score Physics");
+        int ScorePhysics = Keyboard.nextInt();
+        System.out.println("Enter Score Computer science");
+        int ScoreComputer = Keyboard.nextInt();
+        System.out.println("Enter Score Music");
+        int ScoreMusic = Keyboard.nextInt();
+        System.out.println("Enter Score English");
+        int ScoreEnglish = Keyboard.nextInt();
+        System.out.println("Score Math :" + Scoremath);
     }
 }
